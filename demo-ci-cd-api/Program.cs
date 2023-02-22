db23e66a40083d7d395ad8b5713109708e03d704
Rahul Kumar <1999rahul.py@gmail.com>
2023-02-22 16:06:41 +0530

    checking ci-cd action

--- demo-ci-cd-api/Program.cs
+++ demo-ci-cd-api/Program.cs
@@ -8,6 +8,7 @@ namespace demo_ci_cd_api
 
             // Add services to the container.
                //checking
+               //checking
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
