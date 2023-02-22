309b003ac2b742fa438a65589b9761499f6c86e9
Rahul Kumar <1999rahul.py@gmail.com>
2023-02-22 14:20:53 +0530

    minor changes

--- demo-ci-cd-api/Program.cs
+++ demo-ci-cd-api/Program.cs
@@ -7,7 +7,7 @@ namespace demo_ci_cd_api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+               //checking
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
